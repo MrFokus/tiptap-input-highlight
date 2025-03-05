70a5d217539191259251850d27eecec68e9a4bdd
Глубоков Герман <glubokovgerman@MacBook-Pro-3.local>
2025-03-05 14:27:45 +0300

    o my GOD, it's really?

--- pages/index.vue
+++ pages/index.vue
@@ -1,134 +1,148 @@
 <template>
-  <div>
-    <editor-content :editor="editor" />
-  </div>
+  <div
+    contenteditable="true"
+    class="editor"
+    ref="editor"
+    v-html="content"
+  ></div>
 </template>
 
 <script setup lang="ts">
-import { Editor, EditorContent, mergeAttributes } from "@tiptap/vue-3";
-import { Node } from "@tiptap/core";
-import StarterKit from "@tiptap/starter-kit";
-import Focus from "@tiptap/extension-focus";
+import { ref, onMounted } from "vue";
 
-const InputSpan = Node.create({
-  name: "inputSpan",
-  inline: true,
-  group: "inline",
-  content: "text*",
-  selectable: true,
-  defining: true,
-
-
-  addAttributes() {
-    return {
-      class: {
-        default: "input",
-      },
-    };
-  },
-  parseHTML() {
-    return [
-      {
-        tag: "span.input",
-      },
-    ];
-  },
-
-  renderHTML({ HTMLAttributes }) {
-    return ["span", mergeAttributes(HTMLAttributes), 0];
-  },
-});
-
-const editor = ref();
 const content = ref(
-  "<p>Представь, что ты занимаешь должность <span class='input'>укажите должность</span> в <span class='input'>укажите орган власти</span> , твои цели заключаются в <span class='input'>укажите цели</span> . Твоя задача написать речь для</p><p>выступления на <span class='input'>указать тип и название мероприятия</span> . Данное мероприятие приурочено к <span class='input'>укажите повод</span> . Твоя речь будет о <span class='input'>укажите тематику</span></p>"
+  "<p>Представь, что ты занимаешь должность <span tabindex='0' class='input'>укажите должность</span> в <span class='input'>укажите орган власти</span> , твои цели заключаются в <span class='input'>укажите цели</span> . Твоя задача написать речь для</p><p>выступления на <span class='input'>указать тип и название мероприятия</span> . Данное мероприятие приурочено к <span class='input'>укажите повод</span> . Твоя речь будет о <span class='input'>укажите тематику</span></p>"
 );
 
+const editor = ref<HTMLElement | null>(null);
+const currentInput = ref();
+const highlightSpan = (event) => {
+  if (editor.value) {
+    const selection = window.getSelection();
+    console.log(selection);
+
+    if (!selection || !selection.rangeCount) return;
+
+    const range = selection.getRangeAt(0);
+    const selectedNode = range.startContainer;
+    if (selectedNode && selectedNode.nodeType === Node.TEXT_NODE) {
+      const spans = editor.value.querySelectorAll("span.input");
+      spans.forEach((span) => span.classList.remove("focused"));
+
+      if (
+        (selectedNode.parentNode as HTMLElement).tagName === "SPAN" &&
+        (selectedNode.parentNode as HTMLElement).classList.contains("input")
+      ) {
+        currentInput.value = selectedNode.parentNode as HTMLElement;
+
+        if (!currentInput.value.initialValue) {
+          currentInput.value.initialValue = currentInput.value.innerText;
+        }
+        currentInput.value.classList.add("focused");
+        if (!currentInput.value.innerText.startsWith("\u200B")) {
+          currentInput.value.innerText =
+            "\u200B" + currentInput.value.innerText;
+        }
+        if (
+          currentInput.value.initialValue ==
+          currentInput.value.innerText.replaceAll("\u200B", "")
+        ) {
+          moveCaretToStart(currentInput.value);
+        }
+        const blockedKeys = ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"];
+        if (
+          blockedKeys.includes(event.key) &&
+          currentInput.value.initialValue ==
+            currentInput.value.innerText.replaceAll("\u200B", "")
+        ) {
+          event.preventDefault();
+        }
+      } else {
+        currentInput.value = null;
+      }
+    }
+  }
+};
+const moveCaretToStart = (span) => {
+  if (editor.value) {
+    if (span) {
+      const range = document.createRange();
+      const selection = window.getSelection();
+
+      range.setStart(span.firstChild!, 1);
+      range.collapse(true);
+
+      selection?.removeAllRanges();
+      selection?.addRange(range);
+    }
+  }
+};
+const moveCaretToEnd = (span) => {
+  if (editor.value) {
+    if (span) {
+      const range = document.createRange();
+      const selection = window.getSelection();
+
+      range.setStart(span.firstChild!, span.firstChild.length);
+      range.collapse(true);
+
+      selection?.removeAllRanges();
+      selection?.addRange(range);
+    }
+  }
+};
+
 onMounted(() => {
-  editor.value = new Editor({
-    extensions: [StarterKit, Focus, InputSpan],
-    content: content.value,
-    onUpdate: () => {
-      content.value = editor.value.getHTML();
-    },
-  });
-  const inputs = Array.from(
-    editor.value.view.dom.querySelectorAll("span.input")
-  );
-  console.log(inputs);
+  editor.value?.addEventListener("click", highlightSpan);
+  editor.value?.addEventListener("keydown", highlightSpan);
+  editor.value?.addEventListener("input", (ev) => {
+    if (currentInput.value) {
+      console.log(currentInput.value);
 
-  inputs.forEach((input) => {
-    input.addEventListener("click", (ev) => {
-      const observer = new MutationObserver((ev) => {
-        console.log('sssgsgg');
-        
-        if(input.isUpdated){
-          input.isUpdated = false
-          return
-        }
-        if(!input.isUpdated){
-          if(input.innerText.replaceAll("\u200B",'') != input.initialValue){
-            input.innerText = input.innerText.replaceAll(input.initialValue,'')
-            console.log(ev[0].oldValue?.length, input.innerText.length);
-            setCaretPosition(input.innerText.length, input);
-            input.isUpdated = true
-          }
-        }
-
-      });
-      if (!input.initialValue) {
-        input.initialValue = input.innerText;
+      if (
+        ev.inputType == "deleteContentBackward" &&
+        currentInput.value.innerText == currentInput.value.initialValue
+      ) {
+        currentInput.value.remove();
       }
-      if (!input.innerText.startsWith("\u200B")) {
-        input.innerText = "\u200B" + input.innerText;
+      if (currentInput.value.innerText.length == 1) {
+        currentInput.value.innerText =
+          "\u200B" + currentInput.value.initialValue;
+        moveCaretToStart(currentInput.value);
+      } else if (currentInput.value.innerText.length == 0) {
+      } else {
+        currentInput.value.innerText = currentInput.value.innerText.replaceAll(
+          currentInput.value.initialValue,
+          ""
+        );
+        moveCaretToEnd(currentInput.value);
       }
-      observer.observe(input, {
-        childList: true,
-        subtree: true,
-        characterDataOldValue: true,
-      });
-      if(input.innerText.replaceAll("\u200B",'') == input.initialValue){
-        setCaretPosition(1, input);
-      }
-    });
+    }
   });
 });
-
-function setCaretPosition(position, el) {
-  const range = document.createRange();
-  const selection = window.getSelection();
-
-  // Получаем первый дочерний элемент div (или можно выбрать другой)
-  const childNode = el.firstChild;
-
-  if (childNode) {
-    range.setStart(childNode, position); // Устанавливаем позицию каретки
-    range.setEnd(childNode, position); // Устанавливаем конец выделения (если нужно)
-    selection.removeAllRanges();
-    selection.addRange(range); // Применяем диапазон
-  }
-}
 </script>
 
 <style scoped>
-:deep() {
-  .tiptap {
-    margin-top: 400px !important;
-    max-width: 720px;
-    max-height: 200px;
-    overflow: auto;
-    margin: auto;
-    background-color: #f2f4f7;
-    border-radius: 22px;
-    padding: 12px 16px;
-    line-height: 34px;
-    font-family: Inter;
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 400;
-  }
-  .tiptap:focus,
-  span.input:focus {
+.editor {
+  max-width: 720px;
+  max-height: 200px;
+  overflow: auto;
+  background-color: #f2f4f7;
+  border-radius: 22px;
+  padding: 12px 16px;
+  line-height: 34px;
+  font-family: Inter;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+}
+.editor:focus {
+  outline: none;
+  border: none;
+}
+::v-deep() {
+  .editor:focus,
+  span.input.focused {
     outline: none;
     border: none;
   }
@@ -141,14 +155,22 @@ function setCaretPosition(position, el) {
     -webkit-box-decoration-break: clone;
     box-decoration-break: clone;
   }
-  span.input:focus,
-  span.input.has-focus {
+  span.input.focused {
     color: rgba(4, 92, 251, 0.4);
   }
 }
 </style>
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Onest:wght@100..900&display=swap");
+html, #__nuxt, body{
+  min-height: 100dvh;
+}
+#__nuxt{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 * {
   margin: 0;
   padding: 0;
